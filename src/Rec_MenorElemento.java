0d55c343128f95aaa0c9de798c9d4cb196eb5eb4
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-04-20 21:43:52 -0300

    Solução iterativa

--- src/Rec_MenorElemento.java
+++ src/Rec_MenorElemento.java
@@ -1,9 +1,29 @@
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class Rec_MenorElemento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SecureRandom geradorDeNumeros = new SecureRandom();
+		int[] vetor = new int[10];
+		
+		for(int i = 0; i < vetor.length; i++) {
+			vetor[i] = geradorDeNumeros.nextInt(90);
+		}
+		
+		System.out.println(Arrays.toString(vetor));
+		System.out.println(encontrarMenor(vetor));
 
 	}
-
+	
+	public static int encontrarMenor(int[] vetor) {
+		int menor = vetor[0];
+		for(int i = 0; i < vetor.length; i++) {
+			if(vetor[i] < menor) {
+				menor = vetor[i];
+			}
+		}
+		return menor;
+	}
 }
