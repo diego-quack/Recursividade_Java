2b26263bd7e3813d569ae3fa2f0f23fe5a4378db
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-04-22 14:43:10 -0300

    Método 'mostrar' pra faciliar a visualização das saídas

--- src/Rec_MenorElemento.java
+++ src/Rec_MenorElemento.java
@@ -4,84 +4,64 @@ import java.util.Arrays;
 public class Rec_MenorElemento {
 
 	public static void main(String[] args) {
-		
+
 		SecureRandom geradorDeNumeros = new SecureRandom();
 		int[] vetor = new int[10];
-		
-		for(int i = 0; i < vetor.length; i++) {
+
+		for (int i = 0; i < vetor.length; i++) {
 			vetor[i] = geradorDeNumeros.nextInt(90);
 		}
-		
-		System.out.println(Arrays.toString(vetor));
-		System.out.println("O menor elemento da lista é " + encontrarMenorRec(vetor, 0, vetor.length - 1));
 
+		System.out.println(Arrays.toString(vetor));
+		for (int i = 0; i < vetor.length; i++) {
+			encontrarMenorMaior(vetor, 0);
+		}
+		System.out.println("O menor elemento da lista é " + encontrarMenorMaior(vetor, 0));
 	}
-	
+
 	public static int encontrarMenor(int[] vetor) {
 		int menor = vetor[0];
-		for(int i = 0; i < vetor.length; i++) {
-			if(vetor[i] < menor) {
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] < menor) {
 				menor = vetor[i];
 			}
 		}
 		return menor;
 	}
-	
-	public static int encontrarMenorMaior(int[] vetor) {
+
+	public static int[] encontrarMenorMaior(int[] vetor, int n) {
 		
-	}
-	
-	public static int encontrarMenorRec(int[] vetor, int limInferior, int limSuperior) {
-		
-		if(limInferior == limSuperior) {
-			return vetor[limInferior];
+		int maior = vetor[0];
+		int menor = vetor[0];
+		for (int i = 0; i < n; i++) {
+			if (vetor[i] > maior) {
+				maior = vetor[i];
+			} else if (vetor[i] < menor) {
+				menor = vetor[i];
+			}
 		}
-		else {
-			int meio = (limInferior + limSuperior) /2;
+		int[] menorMaior = new int[2];
+		menorMaior[0] = maior;
+		menorMaior[1] = menor;
+
+		return menorMaior;
+	}
+
+	public static int encontrarMenorRec(int[] vetor, int limInferior, int limSuperior) {
+
+		if (limInferior == limSuperior) {
+			return vetor[limInferior];
+		} else {
+			int meio = (limInferior + limSuperior) / 2;
 			int menorA = encontrarMenorRec(vetor, limInferior, meio);
 			int menorB = encontrarMenorRec(vetor, meio + 1, limSuperior);
-			
-			if(menorA < menorB) {
+
+			if (menorA < menorB) {
 				return menorA;
-			}
-			else {
+			} else {
 				return menorB;
 			}
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
