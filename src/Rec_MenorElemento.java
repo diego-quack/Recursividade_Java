b3ef4229f07aa76e10682d6fb272ada6d9c50c6a
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-04-25 17:18:12 -0300

    Versão recursiva encontrarMenorMaior - método menos eficiente que a solução iterativa.

--- src/Rec_MenorElemento.java
+++ src/Rec_MenorElemento.java
@@ -51,8 +51,48 @@ public class Rec_MenorElemento {
 		System.out.println("Maior elemento:" + maior + " - Menor elemento: " + menor);
 	}
 	
-	public static void encontrarMaiorMenorRec(int[] vetor, int tamanho) {
+	public static int[] encontrarMaiorMenorRec(int[] vetor, int limInferior, int limSuperior) {
 		
+		int maior = vetor[0];
+		int menor = vetor[0];
+		
+		if(vetor[limInferior] - vetor[limSuperior] <= 1) {
+			if(vetor[limInferior] < vetor[limSuperior]) {
+				menor = vetor[limSuperior];
+				maior = vetor[limInferior];
+			}
+			else {
+				menor = vetor[limInferior];
+				menor = vetor[limSuperior];
+			}
+		}
+		else {
+			int meio = (limInferior + limSuperior) / 2;
+			int maiorMenor = encontrarMaiorMenorRec(vetor, limInferior, meio);
+			int maiorA = vetor[0];
+			int menorA = vetor[1];
+			int menorMaior = encontrarMaiorMenorRec(vetor, meio + 1, limSuperior);
+			int maiorB = vetor[0];
+			int menorB = vetor[0];
+			
+			if(maiorA > maiorB) {
+				maior = maiorA;
+			}
+			else {
+				maior = maiorB;
+			}
+			if(menorA < menorB) {
+				menor = menorA;
+			}
+			else {
+				menor = menorB;
+			}
+		}
+		int[] menorMaior = new int[2];
+		menorMaior[0] = maior;
+		menorMaior[1] = menor;
+		
+		return menorMaior;
 	}
 
 	public static int encontrarMenorRec(int[] vetor, int limInferior, int limSuperior) {
